304cf3519a3aa50278184cd8f8b2e92612fdb83f
Zuodian Hu <zhu48@wisc.edu>
2016-12-15 14:03:37 -0600

    merge

--- Project/tron.h
+++ Project/tron.h
@@ -1,6 +1,11 @@
 #include "lcd_images.h"
 #include "lcd.h"
 
+#define SELF_FG   LCD_COLOR_BLUE2
+#define SELF_BG   LCD_COLOR_BLACK
+#define REMOTE_FG LCD_COLOR_RED
+#define REMOTE_BG LCD_COLOR_BLACK
+
 void tron_draw_up(unsigned int curr_x, unsigned int curr_y, bool remote);
 void tron_draw_down(unsigned int curr_x, unsigned int curr_y, bool remote);
 void tron_draw_left(unsigned int curr_x, unsigned int curr_y, bool remote);
