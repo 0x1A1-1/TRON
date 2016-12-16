e6472a0925c64a0dfaa3537a25e3c5844004dfb2
Cigarent <xiaojunh1996@hotmail.com>
2016-12-15 19:55:14 -0600

    turning function

--- Project/tron.c
+++ Project/tron.c
@@ -2,13 +2,13 @@
 
 void tron_draw_up(unsigned int curr_x, unsigned int curr_y, bool remote) {
 	uint16_t fg_color;
-	
+
 	if (remote) {
 		fg_color = LCD_COLOR_RED;
 	} else {
 		fg_color = LCD_COLOR_BLUE2;
 	}
-	
+
 	lcd_draw_image(
 		curr_x-5,
 		10,
@@ -22,13 +22,13 @@ void tron_draw_up(unsigned int curr_x, unsigned int curr_y, bool remote) {
 
 void tron_draw_down(unsigned int curr_x, unsigned int curr_y, bool remote) {
 	uint16_t fg_color;
-	
+
 	if (remote) {
 		fg_color = LCD_COLOR_RED;
 	} else {
 		fg_color = LCD_COLOR_BLUE2;
 	}
-	
+
 	lcd_draw_image(
 		curr_x-5,
 		10,
@@ -42,13 +42,13 @@ void tron_draw_down(unsigned int curr_x, unsigned int curr_y, bool remote) {
 
 void tron_draw_left(unsigned int curr_x, unsigned int curr_y, bool remote) {
 	uint16_t fg_color;
-	
+
 	if (remote) {
 		fg_color = LCD_COLOR_RED;
 	} else {
 		fg_color = LCD_COLOR_BLUE2;
 	}
-	
+
 	lcd_draw_image(
 		curr_x,
 		20,
@@ -62,13 +62,13 @@ void tron_draw_left(unsigned int curr_x, unsigned int curr_y, bool remote) {
 
 void tron_draw_right(unsigned int curr_x, unsigned int curr_y, bool remote) {
 	uint16_t fg_color;
-	
+
 	if (remote) {
 		fg_color = LCD_COLOR_RED;
 	} else {
 		fg_color = LCD_COLOR_BLUE2;
 	}
-	
+
 	lcd_draw_image(
 		curr_x-20,
 		20,
@@ -80,6 +80,60 @@ void tron_draw_right(unsigned int curr_x, unsigned int curr_y, bool remote) {
 	);
 }
 
+void turn_hor_from_ver(unsigned int curr_x, unsigned int curr_y, bool up, bool remote){
+	uint16_t fg_color;
+	uint8_t [] trail;
+
+	if (remote) {
+		fg_color = LCD_COLOR_RED;
+	} else {
+		fg_color = LCD_COLOR_BLUE2;
+	}
+
+	if (up){
+		trail = ver_trail_for_up;
+	} else {
+		trail = ver_trail_for_down;
+	}
+
+	lcd_draw_image(
+			curr_x,                 // X Pos
+			imageWidthPixels,   // Image Horizontal Width
+			curr_y,                 // Y Pos
+			imageHeightPixels,  // Image Vertical Height
+			trail,       // Image
+			fg_color,      // Foreground Color
+			LCD_COLOR_BLACK     // Background Color
+		);
+}
+void turn_ver_from_hor(unsigned int curr_x, unsigned int curr_y, bool left, bool remote){
+	uint16_t fg_color;
+	uint8_t [] trail;
+
+	if (remote) {
+		fg_color = LCD_COLOR_RED;
+	} else {
+		fg_color = LCD_COLOR_BLUE2;
+	}
+
+	if (left){
+		trail = hor_trail_for_left;
+	} else {
+		trail = hor_trail_for_right;
+	}
+
+	lcd_draw_image(
+			curr_x,                 // X Pos
+			imageHeightPixels,   // Image Horizontal Width
+			curr_x,                 // Y Pos
+			imageWidthPixels,  // Image Vertical Height
+			trail,       // Image
+			fg_color,      // Foreground Color
+			LCD_COLOR_BLACK     // Background Color
+		);
+}
+
+
 void tron_update_bitmap_lr(unsigned int curr_x, unsigned int curr_y, uint32_t bitmap[240][10]) {
 	bitmap[curr_x][(int)((curr_y-5)/32)] |= 0x03FF << ((curr_y-5) % 32);
 	bitmap[curr_x][(int)((curr_y-4)/32)] |= 0x03FF << ((curr_y-4) % 32);
@@ -87,7 +141,7 @@ void tron_update_bitmap_lr(unsigned int curr_x, unsigned int curr_y, uint32_t bi
 
 void tron_update_bitmap_ud(unsigned int curr_x, unsigned int curr_y, uint32_t bitmap[240][10]) {
 	int i;
-	
+
 	for (i=curr_x-5; i<curr_x+5; i++) {
 		bitmap[curr_x][(int)(curr_y/32)] |= 0x1 << (curr_y % 32);
 	}
--- Project/tron.h
+++ Project/tron.h
@@ -5,6 +5,8 @@ void tron_draw_up(unsigned int curr_x, unsigned int curr_y, bool remote);
 void tron_draw_down(unsigned int curr_x, unsigned int curr_y, bool remote);
 void tron_draw_left(unsigned int curr_x, unsigned int curr_y, bool remote);
 void tron_draw_right(unsigned int curr_x, unsigned int curr_y, bool remote);
+void turn_hor_from_ver(unsigned int curr_x, unsigned int curr_y, bool up, bool remote);
+void turn_ver_from_hor(unsigned int curr_x, unsigned int curr_y, bool left, bool remote);
 
 void tron_update_bitmap_lr(unsigned int curr_x, unsigned int curr_y, uint32_t bitmap[240][10]);
 void tron_update_bitmap_ud(unsigned int curr_x, unsigned int curr_y, uint32_t bitmap[240][10]);
